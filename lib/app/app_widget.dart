6b6c91462516f4ec6f7d5896a373ff63a8717de2
Cledilson <CledilsonWisp@github.com>
2021-06-22 20:55:05 -0300

    Configuração do google, firebase

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pay_flow/app/modules/login/login_page.dart';
-import 'package:pay_flow/app/modules/splash/splash_page.dart';
 import 'package:pay_flow/shared/themes/appColors.dart';
+
 
 class AppWidget extends StatelessWidget {
   @override
